e7c8a53d4fce0ccdfcdd6972df66e5270307670b
Josue Arreola <josue@heroguest.com>
2024-10-23 23:29:02 -0600

    Initial Commit

---
+++ TareaNavegacion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TareaNavegacion
+//
+//  Created by Josué Arreola on 23/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TareaNavegacion/TareaNavegacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TareaNavegacionApp.swift
+//  TareaNavegacion
+//
+//  Created by Josué Arreola on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TareaNavegacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
